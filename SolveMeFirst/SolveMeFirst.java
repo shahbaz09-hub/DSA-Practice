4715da995b2da3e60065d07c9393b6d207fbcf25
Md shahbaz imam <mdshahbazimam02@gmail.com>
2025-11-16 21:06:20 +0530

    Update SolveMeFirst.java

--- SolveMeFirst/SolveMeFirst.java
+++ SolveMeFirst/SolveMeFirst.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+public class SolveMeFirst {
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(solveMeFirst(a, b));
+    }
+}
